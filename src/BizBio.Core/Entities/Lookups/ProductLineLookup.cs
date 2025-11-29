1280db51a541835ef18dd6681e8a16cfae61bdc1
Shaun Kleyn <shaun.kleyn@outlook.com>
2025-11-29 14:01:49 +0200

    fixed circular error

--- src/BizBio.Core/Entities/Lookups/ProductLineLookup.cs
+++ src/BizBio.Core/Entities/Lookups/ProductLineLookup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace BizBio.Core.Entities.Lookups;
 
 public class ProductLineLookup : EnumLookup
 {
+    [JsonIgnore]
     public ICollection<SubscriptionTier> SubscriptionTiers { get; set; } = new List<SubscriptionTier>();
 }
